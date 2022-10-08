752bfad539104236efc5c37a7f85fceb4997e36d
Seong-Gyeong <heather128@ewhain.net>
2022-10-08 17:14:22 +0900

    걍 etc 해거

---
+++ todoList/todoList.js
@@ -0,0 +1,7 @@
+function main() {
+  todayBtn = document.querySelector(".nav_todayOnly");
+  tmrBtn = document.querySelector(".nav_tmrOnly");
+  todayBtn = document.querySelector(".nav_todayOnly");
+}
+
+main();
