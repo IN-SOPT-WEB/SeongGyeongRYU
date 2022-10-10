298c9072adc0cfd2ca592caccfbcce67db577e35
Seong-Gyeong <heather128@ewhain.net>
2022-10-10 18:50:42 +0900

    [Add] add eventlisteners to nav buttons

--- todoList/todoList.js
+++ todoList/todoList.js
@@ -1,7 +1,28 @@
-function main() {
-  todayBtn = document.querySelector(".nav_todayOnly");
-  tmrBtn = document.querySelector(".nav_tmrOnly");
-  todayBtn = document.querySelector(".nav_todayOnly");
-}
+//1. 오늘만 보기, 내일만 보기, 함께 보기 버튼에 따라 레이아웃을 달리 합니다
 
-main();
+leftSection = document.querySelector(".main_left");
+rightSection = document.querySelector(".main_right");
+todayBtn = document.querySelector(".nav_todayOnly");
+tmrBtn = document.querySelector(".nav_tmrOnly");
+allBtn = document.querySelector(".nav_all");
+
+todayBtn.addEventListener("click", () => {
+  // rightSection 없어지게
+  rightSection.style.width = "0%";
+
+  // leftSection 넓어지게
+  leftSection.style.width = "100%";
+});
+
+tmrBtn.addEventListener("click", () => {
+  //leftSection 없어지게
+  leftSection.style.width = "0%";
+
+  // rightSection 넓어지게
+  rightSection.style.width = "100%";
+});
+
+allBtn.addEventListener("click", () => {
+  leftSection.style.width = "50%";
+  rightSection.style.width = "50%";
+});
