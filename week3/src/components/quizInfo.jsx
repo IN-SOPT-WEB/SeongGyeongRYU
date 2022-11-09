e0fdb1873daa0eeda458e441ecfbc3b962b4da14
Seong-Gyeong <heather128@ewhain.net>
2022-11-09 15:24:21 +0900

    코드리뷰 반영

--- week3/src/components/quizInfo.jsx
+++ week3/src/components/quizInfo.jsx
@@ -7,37 +7,41 @@ import chenle from "../img/천러.JPG";
 import jisung from "../img/지성.jpeg";
 
 const quizList = [
-  { src: mark, ans: "마크", option: ["마크", "재민", "천러", "런쥔", "지성"] },
+  {
+    src: mark,
+    answer: "마크",
+    optionList: ["마크", "재민", "천러", "런쥔", "지성"],
+  },
   {
     src: renjun,
-    ans: "런쥔",
-    option: ["런쥔", "제노", "천러", "해찬", "지성"],
+    answer: "런쥔",
+    optionList: ["런쥔", "제노", "천러", "해찬", "지성"],
   },
-  { src: jeno, ans: "제노", option: ["제노", "재민", "마크", "런쥔", "지성"] },
+  {
+    src: jeno,
+    answer: "제노",
+    optionList: ["제노", "재민", "마크", "런쥔", "지성"],
+  },
   {
     src: haechan,
-    ans: "해찬",
-    option: ["천러", "해찬", "천러", "런쥔", "마크"],
+    answer: "해찬",
+    optionList: ["천러", "해찬", "천러", "런쥔", "마크"],
   },
   {
     src: jaemin,
-    ans: "재민",
-    option: ["제노", "재민", "해찬", "마크", "지성"],
+    answer: "재민",
+    optionList: ["제노", "재민", "해찬", "마크", "지성"],
   },
   {
     src: chenle,
-    ans: "천러",
-    option: ["천러", "재민", "마크", "제노", "지성"],
+    answer: "천러",
+    optionList: ["천러", "재민", "마크", "제노", "지성"],
   },
   {
     src: jisung,
-    ans: "지성",
-    option: ["제노", "재민", "천러", "런쥔", "지성"],
+    answer: "지성",
+    optionList: ["제노", "재민", "천러", "런쥔", "지성"],
   },
 ];
 
-const optionList = quizList.map((option) => {
-  return option.ans;
-});
-
-export { quizList, optionList };
+export { quizList };
